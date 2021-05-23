07a43aaa52b15d6838047058acf79c79ba2365f4
Maxime <59308346+MGNetworking@users.noreply.github.com>
2021-05-23 22:36:12 +0200

    mise a jour du fichier properties

--- src/main/java/fr/sid/eurekdiscovery/EurekDiscoveryApplication.java
+++ src/main/java/fr/sid/eurekdiscovery/EurekDiscoveryApplication.java
@@ -2,11 +2,14 @@ package fr.sid.eurekdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekDiscoveryApplication {
+
+    // extends SpringBootServletInitializer
 
     public static void main(String[] args) {
 
