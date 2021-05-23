b9ef3366c7a400576a1e33898f5c85bb32baef85
Maxime <59308346+MGNetworking@users.noreply.github.com>
2021-05-23 03:03:09 +0200

    1er commit

---
+++ src/test/java/fr/sid/eurekdiscovery/EurekDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sid.eurekdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
