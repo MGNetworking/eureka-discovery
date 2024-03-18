7b34ad9884042b196f7c7863a4a446d636d29336
Maxime <ghalem.maxime@gmail.com>
2024-03-18 16:39:45 +0100

    update variable SERVICE_CONFIG_URI to environment

--- script/wait_for_config.sh
+++ script/wait_for_config.sh
@@ -2,8 +2,17 @@
 
 echo  "Lancement du script wait_for_config en cours ... "
 
+if [ -z "$PROFILE_ACTIF_SPRING" ]; then
+
+  echo "La variable PROFILE_ACTIF_SPRING => $PROFILE_ACTIF_SPRING <= est absente "
+  PROFILE_ACTIF_SPRING=nas
+  echo "La variable PROFILE_ACTIF_SPRING est maintenant initialiser => $PROFILE_ACTIF_SPRING <="
+fi
+
+SERVICE_CONFIG_DOCKER=http://ms-configuration:8089
+
   while true; do
-    #response=$(curl -s $SERVICE_CONFIG_DOCKER/msarticle/dev)
+
     response=$(curl -s $SERVICE_CONFIG_DOCKER/msarticle/$PROFILE_ACTIF_SPRING)
 
     echo "request vers : $SERVICE_CONFIG_DOCKER/msarticle/dev"
