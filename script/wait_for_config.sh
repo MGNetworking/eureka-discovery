57b25f83a234dd42b9f7a00110ecd5b3807f1438
Maxime <ghalem.maxime@gmail.com>
2024-03-20 11:03:38 +0100

    modif script wait for config : requeste to config serveur for eureka nas

--- script/wait_for_config.sh
+++ script/wait_for_config.sh
@@ -6,16 +6,16 @@ if [ -z "$PROFILE_ACTIF_SPRING" ]; then
 
   echo "La variable PROFILE_ACTIF_SPRING => $PROFILE_ACTIF_SPRING <= est absente "
   PROFILE_ACTIF_SPRING=nas
-  echo "La variable PROFILE_ACTIF_SPRING est maintenant initialiser => $PROFILE_ACTIF_SPRING <="
+  echo "La variable PROFILE_ACTIF_SPRING est maintenant initialiser => $PROFILE_ACTIF_SPRING "
 fi
 
 SERVICE_CONFIG_DOCKER=http://ms-configuration:8089
 
   while true; do
 
-    response=$(curl -s $SERVICE_CONFIG_DOCKER/msarticle/$PROFILE_ACTIF_SPRING)
+    response=$(curl -s $SERVICE_CONFIG_DOCKER/eureka/$PROFILE_ACTIF_SPRING)
 
-    echo "request vers : $SERVICE_CONFIG_DOCKER/msarticle/dev"
+    echo "request vers : $SERVICE_CONFIG_DOCKER/eureka/$PROFILE_ACTIF_SPRING"
     echo "SERVICE_CONFIG_DOCKER : $SERVICE_CONFIG_DOCKER"
     echo "PROFILE_ACTIF_SPRING: $PROFILE_ACTIF_SPRING"
 
